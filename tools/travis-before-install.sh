fa88ac2af6a262820ed1dfc33243e983eda05586
Jamal I. Mustafa <jimustafa@gmail.com>
2017-11-22 23:55:20 -0800

    add travis script

---
+++ tools/travis-before-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd "$HOME"
+wget http://wannier.org/code/wannier90-2.0.1.tar.gz
+tar zxvf wannier90-2.0.1.tar.gz
+cd wannier90-2.0.1 && touch make.sys && F90=gfortran LIBS="-llapack -lblas" make
