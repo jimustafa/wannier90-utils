19bb42b2144268930eddf87d5f01605f9cc1c08d
Jamal I. Mustafa <jimustafa@gmail.com>
2017-11-25 14:31:23 -0800

    DOC: small tweaks
    - fix some typos
    - add math support
    - document imported members in the I/O subpackage
    - add autosummary extension for numpydoc integration
    - add make target for building live docs with sphinx-autobuild

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,12 @@ sys.path.insert(0, os.path.abspath('../src'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.imgmath',
     'numpydoc',
 ]
+
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
