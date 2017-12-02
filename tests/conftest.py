27c8b30ef56de0fc2f314a0497eac1958f207652
Jamal I. Mustafa <jimustafa@gmail.com>
2017-12-01 16:23:03 -0800

    TST: implement testing against multiple versions of wannier90
    - streamline wannier90 installations
    - condense makefile for building test data
    - update pytest fixtures for multiple versions of wannier90

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,16 @@ import shutil
 import pytest
 
 
-@pytest.fixture(scope='session')
-def data_dir(tmpdir_factory):
+example_ids = {
+    'wannier90-2.0.1': [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 17, 18, 19, 20],
+    'wannier90-2.1.0': [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 17, 18, 19, 20],
+}
+
+
+@pytest.fixture(scope='session', params=['wannier90-2.0.1', 'wannier90-2.1.0'])
+def data_dir(request, tmpdir_factory):
     data_dir = os.path.join(os.path.dirname(__file__), './_data')
-    for example_dir in [('example%02d' % i) for i in [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 13, 17, 18, 19, 20]]:
+    for example_dir in [('%s/example%02d' % (request.param, i)) for i in example_ids[request.param]]:
         shutil.copytree(os.path.join(data_dir, example_dir), os.path.join(str(tmpdir_factory.getbasetemp()), example_dir))
 
-    return str(tmpdir_factory.getbasetemp())
+    return os.path.join(str(tmpdir_factory.getbasetemp()), request.param)
